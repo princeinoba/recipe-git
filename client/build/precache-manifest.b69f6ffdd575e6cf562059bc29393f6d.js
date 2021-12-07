5cde67f4598758c434a1689363cde5c0caf3d85e
Prince Inoba <royceinoba@gmail.com>
2021-12-07 09:44:18 -0500

    Initial Commit

---
+++ client/build/precache-manifest.b69f6ffdd575e6cf562059bc29393f6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c71151b5bac0753539c16aa93e81d346",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a64709a9b35ea51888a3",
+    "url": "/static/css/main.6f68df7b.chunk.css"
+  },
+  {
+    "revision": "751788507483e07c4809",
+    "url": "/static/js/2.98f725f5.chunk.js"
+  },
+  {
+    "revision": "9be8f09f0bfe1d20164d4c42e389b466",
+    "url": "/static/js/2.98f725f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a64709a9b35ea51888a3",
+    "url": "/static/js/main.effc5ed4.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  }
+]);
